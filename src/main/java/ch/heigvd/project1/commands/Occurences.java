d8dce0f7df17ab98c38dd8b602ad90c727c5be01
QuentinEschmann <qeschmann@icloud.com>
2025-10-05 18:27:29 +0200

    feat: wrote 2 subcommands (stats & occurences)

--- src/main/java/ch/heigvd/project1/commands/Occurences.java
+++ src/main/java/ch/heigvd/project1/commands/Occurences.java
@@ -1,6 +1,8 @@
 package ch.heigvd.project1.commands;
 
 import picocli.CommandLine;
+import java.util.ArrayList;
+import java.util.List;
 
 @CommandLine.Command(name = "occurences", description = "Find all the words occurences in a text file")
 public class Occurences implements Runnable {
@@ -13,6 +15,19 @@ public class Occurences implements Runnable {
     protected String word;
 
     public void run(){
-        System.out.println("Occurences of " + word);
+        String input = "bonjour la suisse bonjourtest la bonjour labonjourbonjour";
+        List<Integer> indexes = new ArrayList<>();
+        String originalInput = input;
+        int offset = 0;
+
+        while(input.indexOf(word) != -1){
+            int foundIndex = input.indexOf(word);
+            indexes.add(offset + foundIndex);
+            
+            offset += foundIndex + word.length();
+            input = input.substring(foundIndex + word.length());
+        }
+
+        System.out.println("There are " + indexes.size() + " occurences of the word \"" + word + "\". They are at positions: " + indexes);
     }    
 }
