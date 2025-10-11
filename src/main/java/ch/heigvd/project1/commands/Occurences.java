d4f0b35cc03695e1a8647b098080737e9d824adc
aihxpos111 <sofia@LAPTOP-KFTE5POT.localdomain>
2025-10-11 17:02:08 +0200

    #17

--- src/main/java/ch/heigvd/project1/commands/Occurences.java
+++ src/main/java/ch/heigvd/project1/commands/Occurences.java
@@ -1,11 +1,18 @@
 package ch.heigvd.project1.commands;
 
+import ch.heigvd.project1.ios.Read;
+import ch.heigvd.project1.ios.Write;
 import picocli.CommandLine;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 @CommandLine.Command(name = "occurences", description = "Find all the words occurences in a text file")
 public class Occurences implements Runnable {
+
+    @CommandLine.ParentCommand protected Root parent;
 
     @CommandLine.Option(
         names = {"-w", "--word"},
@@ -15,19 +22,26 @@ public class Occurences implements Runnable {
     protected String word;
 
     public void run(){
-        String input = "bonjour la suisse bonjourtest la bonjour labonjourbonjour";
-        List<Integer> indexes = new ArrayList<>();
-        String originalInput = input;
-        int offset = 0;
+            String input = Read.readFile(parent.getIFileName());
+            List<Integer> indexes = new ArrayList<>();
+            int offset = 0;
 
-        while(input.indexOf(word) != -1){
-            int foundIndex = input.indexOf(word);
-            indexes.add(offset + foundIndex);
-            
-            offset += foundIndex + word.length();
-            input = input.substring(foundIndex + word.length());
-        }
+            while (input.contains(word)) {
+                int foundIndex = input.indexOf(word);
+                indexes.add(offset + foundIndex);
 
-        System.out.println("There are " + indexes.size() + " occurences of the word \"" + word + "\". They are at positions: " + indexes);
+                offset += foundIndex + word.length();
+                input = input.substring(foundIndex + word.length());
+            }
+
+            try{
+                BufferedWriter br = Write.open(parent.getOFileName());
+                br.write("There are " + indexes.size() + " occurences of the word \"" + word + "\". They are at positions: " + indexes);
+                br.close();
+            } catch (IOException e) {
+                System.err.println("Error: " + e.getMessage());
+                System.exit(-1);
+            }
+
     }    
 }
--- src/main/java/ch/heigvd/project1/ios/Read.java
+++ src/main/java/ch/heigvd/project1/ios/Read.java
@@ -8,6 +8,7 @@ public class Read {
     //source : https://stackoverflow.com/questions/4716503/reading-a-plain-text-file-in-java
     public static String readFile(String fileName){
         StringBuilder sb = new StringBuilder();
+        String text = null;
 
         try ( BufferedReader br = new BufferedReader(new FileReader(fileName, StandardCharsets.UTF_8)) ){
             String line = br.readLine();
@@ -18,10 +19,13 @@ public class Read {
                 line = br.readLine();
             }
 
-            return sb.toString();
+            text =  sb.toString();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            System.err.println("Error: " + e.getMessage());
+            System.exit(-1);
         }
+
+        return text;
     }
 
 }
