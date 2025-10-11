768b694e2f311634830ef3f56b7f813020f42a5f
aihxpos111 <sofia@LAPTOP-KFTE5POT.localdomain>
2025-10-11 16:19:13 +0200

    change Test.java into AppTest.java

--- src/test/java/ch/heigvd/project1/AppTest.java
+++ src/test/java/ch/heigvd/project1/AppTest.java
@@ -1,8 +1,11 @@
 package ch.heigvd.project1;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import ch.heigvd.project1.ios.Read;
+import ch.heigvd.project1.ios.Write;
 import org.junit.jupiter.api.Test;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
 
 /**
  * Unit test for simple App.
@@ -13,7 +16,16 @@ public class AppTest {
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void readAndWrite() throws IOException {
+        String string = Read.readFile("src/test/java/ch/heigvd/project1/Input.txt");
+        System.out.println(string);
+
+        try{
+            BufferedWriter bw = Write.open("src/test/java/ch/heigvd/project1/Output.txt");
+            bw.write(string);
+            Write.close(bw);
+        } catch ( IOException e){
+            throw new IOException(e);
+        }
     }
 }
